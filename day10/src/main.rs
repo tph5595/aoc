0fec7ab911db72e6794767196a5c7ee026afffa1
Taylor Henderson <tph5595@verizon.net>
2024-11-18 14:31:13 -0500

    setup 2024

--- day10/src/main.rs
+++
@@ -1,8 +0,0 @@
-use day10::{p1, p2, p2_og_idea};
-
-fn main() {
-    let file = "./input.txt";
-    println!("p1: {:?}", p1(file));
-    println!("p2: {}", p2(file));
-    println!("p2 orginal idea: {}", p2_og_idea(file));
-}
