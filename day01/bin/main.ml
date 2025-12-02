5c7ca213bb42c8e57f24bbc4db03052107bcadab
Taylor Henderson <tph5595@verizon.net>
2025-12-01 23:09:12 -0500

    save

--- day01/bin/main.ml
+++ day01/bin/main.ml
@@ -8,7 +8,6 @@ let eq_sign x y =
 let part1 input = 
     let count = 50 in 
     let size = 100 in 
-    (* let straight = *) 
     input 
     |> List.map ~f:(fun x -> 
         let s = String.to_list x in 
@@ -22,20 +21,26 @@ let part1 input =
     |> List.map ~f:(fun x -> if x < 0 then x + size else x)
     |> List.filter ~f:(fun x -> phys_equal x 0)
     |> List.length
-    (* let seen = Hashtbl.create (module Int) in *)
-    (* List.fold_left ~init:0 ~f:(fun l x -> *) 
-    (*     let prev = Hashtbl.find seen x in *) 
-    (*     let times_seen = match prev with *) 
-    (*     | Some x -> x + 1 *)
-    (*     | None -> 1 in *) 
-    (*     Hashtbl.set seen ~key:x ~data:times_seen; *) 
-    (*     let Hashtbl.find seen (size-l)%size *)
-    (*     l + (Hashtbl.find)) straight *)
 
-let part_no input = 
-    let count = 50 in 
-    let size = 100 in 
-    let straight = input 
+    (*
+
+    When to count:
+        - Assume start at positive number
+        - add number
+        - If end positive
+            - every time over 100
+            - if land at zero
+        - If end negative
+            - +1 for negative
+            - everytime under -100
+            - if land at zero
+        - Modulo and make positive before next iteration
+
+     *)
+
+let part_no file = 
+    let start = 50 in 
+    let straight = file 
     |> List.map ~f:(fun x -> 
         let s = String.to_list x in 
         match s with 
@@ -43,23 +48,26 @@ let part_no input =
         | hd :: tl when phys_equal hd 'L' -> (tl |> String.of_list |> Int.of_string ) * -1
         | _ -> 0
     ) in
-
-    let mid1 = straight
-    |> List.map ~f:(fun x -> Int.abs (x / size))
-    |> List.fold_left ~init:0 ~f:(+) in 
-
-    let s2 = straight
-    |> List.fold_left ~init:[count] ~f:(fun l x -> [x + List.hd_exn l]@l)
-    |> List.map ~f:(fun x -> x % size) in 
-
-    let mid2 = s2
-    |> List.map ~f:(fun x -> if x < 0 then 1 else 0)
-    |> List.fold_left ~init:0 ~f:(+) in 
-    let s3 = s2
-    |> List.map ~f:(fun x -> if x < 0 then x + size else x)
-    |> List.filter ~f:(fun x -> phys_equal x 0)
-    |> List.length in 
-    s3, mid1, mid2
+    let ans = straight
+    |> List.fold_left ~init:[(0, start)] ~f:(fun l x -> 
+            let (c, last) = List.hd_exn (List.rev l) in 
+            let new_pos = last + x in 
+            (*Exact hit on 0*)
+            if phys_equal new_pos 0 then
+                l@[(c+1, new_pos)]
+            (* Positive end *)
+            else if new_pos > 0 then
+                l@[(c + (new_pos/100), new_pos%100)]
+            (* Negative end *)
+            else
+                (* If started at zero then no auto +1 for being negative *)
+                let dumb = if phys_equal last 0 then 0 else 1 in 
+                l@[(c + (new_pos/100) + dumb, (new_pos%100))]
+    ) in 
+    ans
+    |> List.rev 
+    |> List.hd_exn
+    |> fst
 
 let part2 input = 
     let start = 50 in 
@@ -83,6 +91,7 @@ let part2 input =
     |> List.fold_left ~init:0 ~f:(fun c x -> c + Int.abs (x/100)) in 
     let (_, between) = turns
     |> List.fold_left ~init:[start] ~f:(fun l x -> [x + List.hd_exn l]@l)
+    |> List.rev
     (* |> List.fold_left ~init:[start] ~f:(fun l x -> [x + List.hd_exn l]@l) *)
 
     (* |> List.fold_left ~init:(0, start) ~f:(fun (count, l) x -> *) 
@@ -102,12 +111,12 @@ let part2 input =
 let read_file (filename : string) : string list =
   In_channel.read_lines filename
 
-let file = "input"
+let file = "test2"
 
 let () = 
     let result = part1 (read_file file ) in 
     Printf.printf "Part 1: %d\n" result;
     let result, r2, r3 = part2(read_file file ) in 
     Printf.printf "Part 2: %d\t%d\t%d\n" result r2 r3;
-    let result, r2, r3 = part_no(read_file file ) in 
-    Printf.printf "Part 2: %d\t%d\t%d\n" result r2 r3;
+    let result = part_no(read_file file ) in 
+    Printf.printf "Part no: %d\n" result;
